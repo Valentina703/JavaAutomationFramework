818ae89a7d0694374bf84f6ca0cd8f332f41a129
Valentina <v.v.begu@gmail.com>
2023-10-18 19:00:19 +0300

    Implemented the FakeDataManager and create the first scenario.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,10 @@
 package com.opencart;
 
 import com.opencart.managers.DriverManager;
+import com.opencart.managers.RandomDataManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 
@@ -12,9 +15,53 @@ public class TestRunner {
         String currentWindowName = driver.getWindowHandle();
 
         driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://www.google.com/");
+        driver.get("https://andreisecuqa.host/");
+        Thread.sleep(3000);
 
-        Thread.sleep(2000);
+        WebElement myAccountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        myAccountIcon.click();
+
+        WebElement registerOption = driver.findElement(By.xpath("//a[@class='dropdown-item'][normalize-space()='Register']"));
+        registerOption.click();
+
+        System.out.println(driver.getCurrentUrl());
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+
+        String firstName = RandomDataManager.generateFirstName();
+        firstNameInput.sendKeys(firstName);
+        System.out.println(firstName);
+
+        WebElement lastNameInput = driver.findElement(By.cssSelector("#input-firstname"));
+
+        String lastName = RandomDataManager.generateLastName();
+        lastNameInput.sendKeys(lastName);
+        System.out.println(lastName);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
+
+        String randomEmail = RandomDataManager.generateRandomEmail();
+        emailInput.sendKeys(randomEmail);
+        System.out.println(randomEmail);
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
+
+        String password = RandomDataManager.generatePassword();
+
+        passwordInput.sendKeys(password);
+        System.out.println(password);
+
+
+        WebElement termsAndConditionCheckBox = driver.findElement(By.xpath("//input[@name='agree']"));
+
+        termsAndConditionCheckBox.click();
+
+        WebElement registerBtn = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+
+        registerBtn.click();
+
+        Thread.sleep(5000);
+
         System.out.println(driver.getTitle());
         driver.close();
 
@@ -22,6 +69,7 @@ public class TestRunner {
         driver.get("https://tekwill.md/");
         Thread.sleep(1000);
         System.out.println(driver.getTitle());
+        driver.close();
         driver.quit();
 
         System.out.println("The execution is over");
